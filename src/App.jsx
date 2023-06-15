89b7342e2e5a48c959d691d8e2f81e2499502c1d
yoshi <mdmdmdmdmd57@gmail.com>
2023-06-15 19:17:21 +0900

    HTMLの雛形を作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 export const App = ()=> {
   return(
     <>
-      <div>todo
-        アプリ
+      <input placeholder="todoを入力"/>
+      <div>
+        <ul>
+          <li>
+            <div>
+              <p>todoリスト</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <div>
+              <p><p>todoリスト</p></p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
       </div>
     </>
 
   )
-}
+};
